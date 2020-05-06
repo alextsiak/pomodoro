385f72511253db1e89a67b5bf8f7a630cddd2994
Alex Tsiakalou <altsiakalou@gmail.com>
2020-05-06 15:45:01 +0300

    Make modal content responsive

--- script.js
+++ script.js
@@ -146,3 +146,8 @@ closeButton.addEventListener("click", function() {
 	modal.style.display = "none";
 })
 
+window.onclick = function(event) {
+	if (event.target == modal) {
+		modal.style.display = "none";
+	}
+}
